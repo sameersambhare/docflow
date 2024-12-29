772949f3c0f572c2394baf1e04dc6b50f9f56157
sameersambhare <sameersambhare@gmail.com>
2024-12-29 14:17:51 +0530

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const Home = () => {
+  return (
+    <div className="">
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Home
