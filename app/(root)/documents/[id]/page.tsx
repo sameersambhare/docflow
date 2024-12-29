67b5d887e9afcacff4d51361c502020a9ff120aa
sameersambhare <sameersambhare@gmail.com>
2024-12-29 17:33:41 +0530

    create document feature

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,10 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
 import React from 'react'
 const Document = () => {
   return (
-    <div>
-    </div>
+    <main className='w-full flex flex-col items-center'>
+      <CollaborativeRoom />
+    </main>
   )
 }
 
