0fed835427dd61ad32c6de26358a2dded44e81db
sameersambhare <sameersambhare@gmail.com>
2024-12-31 17:32:12 +0530

    initial deploy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,17 @@ import Image from 'next/image'
 import AddDocumentBtn from '@/components/AddDocumentBtn'
 import { redirect } from 'next/navigation'
 import { currentUser } from '@clerk/nextjs/server'
+import { getDocuments } from '@/lib/actions/room.actions'
+import Link from 'next/link'
+import { dateConverter } from '@/lib/utils'
+
 const Home = async () => {
   let clerkUser = await currentUser();
   if (!clerkUser) {
     redirect('/sign-in')
   }
-  const documents = []
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress)
+
   return (
     <main className='home-container'>
       <Header className="sticky left-0 top-0">
@@ -21,8 +26,31 @@ const Home = async () => {
           </SignedIn>
         </div>
       </Header>
-      {documents.length > 0 ? (
-        <div className=""></div>
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className='text-28-semibold'>All Documents</h3>
+            <AddDocumentBtn userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress} />
+          </div>
+          <ul className='document-ul'>
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+              <li key={id} className='document-list-item'>
+                <Link href={`/document/${id}`} className='flex flex-1 items-center gap-4'>
+                  <div className="hidden rounded-md p-2 sm:block bg-dark-500">
+                    <Image src='/assets/icons/doc.svg' width={24} height={24} alt='file'></Image>
+                  </div>
+                  <div className="space-y-1">
+                    <p className='line-clamp-1 text-lg'>{metadata.title}</p>
+                    <p className='text-sm font-light text-blue-100'>Created about {dateConverter(createdAt)}</p>
+                  </div>
+                  {
+                    //TODO: Delete button
+                  }
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (<div className="document-list-empty">
         <Image src='/assets/icons/doc.svg' alt='Document' width={40} height={40} className='mx-auto' />
         <AddDocumentBtn userId={clerkUser.id} email={clerkUser.emailAddresses[0].emailAddress} />
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,63 +1,72 @@
-'use server';
+"use server";
 
-import { nanoid } from 'nanoid'
-import { liveblocks } from '../liveblocks';
-import { revalidatePath } from 'next/cache';
-import { getAccessType, parseStringify } from '../utils';
-import { redirect } from 'next/navigation';
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { getAccessType, parseStringify } from "../utils";
+import { redirect } from "next/navigation";
 
-export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
   const roomId = nanoid();
 
   try {
     const metadata = {
       creatorId: userId,
       email,
-      title: 'Untitled'
-    }
+      title: "Untitled",
+    };
 
     const usersAccesses: RoomAccesses = {
-      [email]: ['room:write']
-    }
+      [email]: ["room:write"],
+    };
 
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
-      defaultAccesses: ['room:write']
+      defaultAccesses: ["room:write"],
     });
-    
-    revalidatePath('/');
+
+    revalidatePath("/");
 
     return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while creating a room: ${error}`);
   }
-}
+};
 
-export const getDocument = async ({ roomId, userId }: { roomId: string; userId: string }) => {
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
   try {
-      const room = await liveblocks.getRoom(roomId);
-    
-      //TODO: BRING THIS BACK
-      // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
-    
-      // if(!hasAccess) {
-      //   throw new Error('You do not have access to this document');
-      // }
-    
-      return parseStringify(room);
+    const room = await liveblocks.getRoom(roomId);
+
+    //TODO: BRING THIS BACK
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if(!hasAccess) {
+    //   throw new Error('You do not have access to this document');
+    // }
+
+    return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while getting a room: ${error}`);
   }
-}
+};
 
 export const updateDocument = async (roomId: string, title: string) => {
   try {
     const updatedRoom = await liveblocks.updateRoom(roomId, {
       metadata: {
-        title
-      }
-    })
+        title,
+      },
+    });
 
     revalidatePath(`/documents/${roomId}`);
 
@@ -65,4 +74,12 @@ export const updateDocument = async (roomId: string, title: string) => {
   } catch (error) {
     console.log(`Error happened while updating a room: ${error}`);
   }
-}
+};
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error happened while getting a rooms: ${error}`);
+  }
+};
